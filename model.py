b314f8a1e07bef2753c9a2688ee7850d0c119c65
Juliette <juli.tripier@gmail.com>
2023-09-27 16:59:02 -0400

    Finished Part 1

--- model.py
+++ model.py
@@ -4,8 +4,69 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# 8. A movie can have as many ratings as there are users
+        # A movie has many ratings made by many different users
+# 9. One user can rate multiple movies
+        # A user has many ratings that they make
+# 10. User table has primary key for user ID, movies table has primary key for movie ID, ratings table has a foreign key
+# for user ID and movie ID
 
 # Replace this with your code!
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    ratings = db.relationship("Rating", back_populates="user")
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+
+
+class Movie(db.Model):
+    """A movie."""
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    ratings = db.relationship("Rating", back_populates="movie")
+
+    def __repr__(self):
+        return f'<Movie movie_id={self.movie_id} title={self.title}>'
+    
+
+class Rating(db.Model):
+    """A movie rating."""
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer,
+                         db.ForeignKey('movies.movie_id'))
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.user_id'))
+    
+    movie = db.relationship("Movie", back_populates="ratings")
+    user = db.relationship("User", back_populates="ratings")
+
+
+    def __repr__(self):
+        return f'<Rating rating_id={self.rating_id} score={self.score}>'
 
 
 def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
